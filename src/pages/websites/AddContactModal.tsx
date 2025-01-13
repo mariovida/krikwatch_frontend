07dd6d6354ec496e4f50181f425f9401b70827dc
Mario <mariovidovic97@gmail.com>
2025-01-13 17:09:12 +0100

    Add website contact function + bug fixes

---
+++ src/pages/websites/AddContactModal.tsx
@@ -0,0 +1,250 @@
+import React, { useState, useEffect } from "react";
+import { useUser } from "../../context/UserContext";
+import axios from "axios";
+
+import {
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+} from "@mui/material";
+
+interface AddContactModalProps {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: (user: {
+    first_name: string;
+    last_name: string;
+    email: string;
+  }) => void;
+  editMode: boolean;
+  user?:
+    | {
+        id: number;
+        first_name: string;
+        last_name: string;
+        email: string;
+        is_verified: number;
+        verified: boolean;
+      }
+    | null
+    | undefined;
+  firstName: string;
+  lastName: string;
+  email: string;
+  setFirstName: React.Dispatch<React.SetStateAction<string>>;
+  setLastName: React.Dispatch<React.SetStateAction<string>>;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const AddContactModal: React.FC<AddContactModalProps> = ({
+  open,
+  onClose,
+  onSubmit,
+  editMode = false,
+  user,
+  firstName,
+  lastName,
+  email,
+  setFirstName,
+  setLastName,
+  setEmail,
+}) => {
+  let backendUrl = import.meta.env.VITE_BACKEND_URL;
+  if (import.meta.env.VITE_ENV === "production") {
+    backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
+  }
+
+  const [errors, setErrors] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+  });
+  const { user: currentUser } = useUser();
+
+  useEffect(() => {
+    if (editMode && user) {
+      setFirstName(user.first_name || "");
+      setLastName(user.last_name || "");
+      setEmail(user.email || "");
+    }
+    if (!editMode) {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+    }
+  }, [editMode, user, setFirstName, setLastName, setEmail]);
+
+  const validateFields = () => {
+    let isValid = true;
+    const newErrors = { first_name: "", last_name: "", email: "" };
+
+    if (!firstName.trim()) {
+      newErrors.first_name = "First name is required.";
+      isValid = false;
+    } else if (firstName.length > 50) {
+      newErrors.first_name = "First name must be less than 50 characters.";
+      isValid = false;
+    }
+
+    if (!lastName.trim()) {
+      newErrors.last_name = "Last name is required.";
+      isValid = false;
+    } else if (lastName.length > 50) {
+      newErrors.last_name = "Last name must be less than 50 characters.";
+      isValid = false;
+    }
+
+    if (!email.trim()) {
+      newErrors.email = "Email is required.";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Invalid email format.";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "first_name") setFirstName(value);
+    if (name === "last_name") setLastName(value);
+    if (name === "email") setEmail(value);
+
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
+  };
+
+  const handleSubmit = () => {
+    if (validateFields()) {
+      onSubmit({ first_name: firstName, last_name: lastName, email });
+    }
+  };
+
+  const handleClose = () => {
+    setErrors({ first_name: "", last_name: "", email: "" });
+    if (!editMode) {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+    }
+    onClose();
+  };
+
+  const handleDisableUser = async () => {
+    try {
+      if (user) {
+        const token = localStorage.getItem("accessToken");
+        const response = await axios.put(
+          `${backendUrl}/api/users/${user.id}/toggle-verification`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          window.location.reload();
+        }
+      }
+    } catch (error) {
+      console.error("Error toggling the user's verification status:", error);
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      className="custom-modal"
+      sx={{
+        "& .MuiPaper-root": {
+          maxWidth: "540px",
+          width: "100%",
+        },
+      }}
+    >
+      <DialogTitle>{editMode ? "Edit contact" : "Add new contact"}</DialogTitle>
+      <DialogContent>
+        <Box className="form-fields">
+          <TextField
+            label="First name"
+            name="first_name"
+            value={firstName}
+            onChange={handleInputChange}
+            error={!!errors.first_name}
+            helperText={errors.first_name}
+            fullWidth
+            variant="filled"
+            inputProps={{ maxLength: 50 }}
+            required
+          />
+          <TextField
+            label="Last name"
+            name="last_name"
+            value={lastName}
+            onChange={handleInputChange}
+            error={!!errors.last_name}
+            helperText={errors.last_name}
+            fullWidth
+            variant="filled"
+            inputProps={{ maxLength: 50 }}
+            required
+          />
+          <TextField
+            label="Email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            error={!!errors.email}
+            helperText={errors.email}
+            fullWidth
+            variant="filled"
+            required
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions
+        sx={{
+          justifyContent: "space-between !important",
+        }}
+      >
+        {user && currentUser && currentUser.email !== user.email ? (
+          user.is_verified === 1 ? (
+            <Button className="delete-btn" onClick={handleDisableUser}>
+              Disable user
+            </Button>
+          ) : user.verified ? (
+            <Button
+              className="delete-btn"
+              sx={{ color: "#1b2431 !important" }}
+              onClick={handleDisableUser}
+            >
+              Enable user
+            </Button>
+          ) : (
+            <Button className="delete-btn"></Button>
+          )
+        ) : (
+          <Button className="delete-btn"></Button>
+        )}
+        <div>
+          <Button onClick={handleClose} className="cancel-btn">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} className="submit-btn">
+            {editMode ? "Save changes" : "Create"}
+          </Button>
+        </div>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AddContactModal;
--- src/pages/websites/WebsiteDetailsPage.tsx
+++ src/pages/websites/WebsiteDetailsPage.tsx
@@ -4,9 +4,11 @@ import { Helmet } from "react-helmet-async";
 import axios from "axios";
 
 import styled from "@emotion/styled";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Snackbar, Stack, Typography } from "@mui/material";
 import ArrowLeftIcon from "../../assets/icons/arrow-left.svg";
+import ChevronUp from "../../assets/icons/arrow-up-right.svg";
 
+import AddContactModal from "./AddContactModal";
 import { formatDateWithClock } from "../../helpers/formatDateWithClock";
 
 const WebsiteDetails = styled(Stack)({
@@ -14,6 +16,7 @@ const WebsiteDetails = styled(Stack)({
   flexDirection: "row",
   alignItems: "center",
   gap: "24px",
+  marginBottom: "40px",
 });
 
 const WebsiteDetailsPage = () => {
@@ -26,12 +29,19 @@ const WebsiteDetailsPage = () => {
   const { id } = useParams();
   const [website, setWebsite] = useState<any>(null);
   const [clients, setClients] = useState<any[]>([]);
+  const [contacts, setContacts] = useState<any>(null);
   const [selectedClient, setSelectedClient] = useState<string>("");
   const [websiteName, setWebsiteName] = useState<string>("");
   const [websiteUrl, setWebsiteUrl] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
 
-  console.log(website);
+  const [openContactModal, setOpenContactModal] = useState(false);
+  const [modalModeEdit, setModalModeEdit] = useState<boolean>(false);
+  const [userExistsError, setUserExistsError] = useState<boolean>(false);
+  const [selectedContact, setSelectedContact] = useState<any>(null);
+  const [contactName, setContactName] = useState("");
+  const [contactSurname, setContactSurname] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
 
   useEffect(() => {
     const fetchWebsite = async () => {
@@ -72,9 +82,30 @@ const WebsiteDetailsPage = () => {
       }
     };
 
+    const fetchContacts = async () => {
+      setLoading(true);
+      try {
+        const token = localStorage.getItem("accessToken");
+        const response = await axios.get(
+          `${backendUrl}/api/contacts/website/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setContacts(response.data.contacts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     if (id) {
       fetchWebsite();
       fetchClients();
+      fetchContacts();
     }
   }, [backendUrl, id]);
 
@@ -86,6 +117,98 @@ const WebsiteDetailsPage = () => {
       }
     }
   }, [clients, website]);
+
+  const handleCreateContact = async () => {
+    const newContact = {
+      first_name: contactName,
+      last_name: contactSurname,
+      email: contactEmail,
+      website_id: id,
+    };
+
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await axios.post(
+        `${backendUrl}/api/contacts`,
+        newContact,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response && response.data) {
+        if (
+          response.data.message &&
+          response.data.message ===
+            "Contact with this email address already exists"
+        ) {
+          setUserExistsError(true);
+        }
+      }
+      if (response.status === 201) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error adding contact:", error);
+      alert("Error adding contact.");
+    }
+  };
+
+  const handleUpdateContact = async () => {
+    const updatedContact = {
+      first_name: contactName,
+      last_name: contactSurname,
+      email: contactEmail,
+    };
+
+    if (selectedContact) {
+      try {
+        const token = localStorage.getItem("accessToken");
+        const response = await axios.put(
+          `${backendUrl}/api/contacts/${selectedContact.id}`,
+          updatedContact,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          setContacts((prevContacts: any) =>
+            prevContacts.map((contact: any) =>
+              contact.id === selectedContact.id
+                ? response.data.contact
+                : contact
+            )
+          );
+          setOpenContactModal(false);
+          setSelectedContact(null);
+          setContactName("");
+          setContactSurname("");
+          setContactEmail("");
+
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error("Error updating user:", error);
+      }
+    }
+  };
+
+  const handleOpenContactModal = () => {
+    setOpenContactModal(true);
+  };
+  const handleCloseModal = () => setOpenContactModal(false);
+  const handleSnackbarClose = () => setUserExistsError(false);
+
+  const handleEditUser = (user: React.SetStateAction<null>) => {
+    setSelectedContact(user);
+    setModalModeEdit(true);
+    setOpenContactModal(true);
+  };
 
   const handleClose = () => {
     navigate(`/websites`);
@@ -180,10 +303,116 @@ const WebsiteDetailsPage = () => {
                   </Box>
                 </WebsiteDetails>
               </div>
+              <div className="col-12">
+                <Stack
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    marginBottom: "24px",
+                  }}
+                >
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      fontFamily: "Plus Jakarta Sans, sans-serif",
+                      lineHeight: "40px !important",
+                    }}
+                  >
+                    Contacts
+                  </Typography>
+                  <Button
+                    onClick={handleOpenContactModal}
+                    sx={{
+                      flex: "none",
+                      display: "inline-flex",
+                      alignItems: "center",
+                      color: "#ffffff",
+                      fontSize: "15px",
+                      fontWeight: 600,
+                      lineHeight: "40px",
+                      textDecoration: "none",
+                      textTransform: "none",
+                      backgroundColor: "#1b2431",
+                      padding: "0 16px",
+                      borderRadius: "6px",
+                      boxShadow:
+                        "rgba(0, 0, 0, 0.04) 0px 5px 22px 0px,rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+                      transition: "0.2s",
+                      cursor: "pointer",
+
+                      "&:hover": {
+                        opacity: "0.9",
+                      },
+                    }}
+                  >
+                    Add new
+                  </Button>
+                </Stack>
+              </div>
+              {contacts && (
+                <div className="col-12">
+                  <table className="custom-table">
+                    <thead>
+                      <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Email</th>
+                        <th>Created at</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {contacts.length > 0 ? (
+                        contacts.map((contact: any) => (
+                          <tr key={contact.id}>
+                            <td>{contact.first_name}</td>
+                            <td>{contact.last_name}</td>
+                            <td>{contact.email}</td>
+                            <td>{formatDateWithClock(contact.created_at)}</td>
+                            <td style={{ width: "100px" }}>
+                              <button onClick={() => handleEditUser(contact)}>
+                                <img src={ChevronUp} />
+                              </button>
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td colSpan={6} style={{ textAlign: "center" }}>
+                            No contacts found with the query
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              )}
             </div>
           </div>
         </section>
       )}
+      <AddContactModal
+        open={openContactModal}
+        onClose={handleCloseModal}
+        onSubmit={modalModeEdit ? handleUpdateContact : handleCreateContact}
+        editMode={modalModeEdit}
+        user={selectedContact}
+        firstName={contactName}
+        lastName={contactSurname}
+        email={contactEmail}
+        setFirstName={setContactName}
+        setLastName={setContactSurname}
+        setEmail={setContactEmail}
+      />
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        open={userExistsError}
+        onClose={handleSnackbarClose}
+        message="Contact with this email address already exists."
+        className="snackbar snackbar-error"
+        autoHideDuration={4000}
+      />
     </>
   );
 };
