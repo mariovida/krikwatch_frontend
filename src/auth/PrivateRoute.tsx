7d6debc1d3543e2b6edfff8e44e96f67195c5271
Mario <mariovidovic97@gmail.com>
2025-01-31 15:14:28 +0100

    PrivateRoute updated

--- src/auth/PrivateRoute.tsx
+++ src/auth/PrivateRoute.tsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+
 const LoadingSpinner = () => {
   return (
-    <div className="loading-spinner">
-      <span>Loading...</span>
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress sx={{ height: "6px" }} />
+    </Box>
+    // <div className="loading-spinner">
+    //   <span>Loading...</span>
+    // </div>
   );
 };
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const [loading, setLoading] = useState(true);
+  const [showTest, setShowTest] = useState(true);
   const [authToken, setAuthToken] = useState<string | null>(null);
 
   useEffect(() => {
