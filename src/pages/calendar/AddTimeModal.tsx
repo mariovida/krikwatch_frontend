22c13164615df8bd065531d38a6d9e51e25b2c9b
Mario <mariovidovic97@gmail.com>
2025-02-03 16:48:22 +0100

    Calendar

--- src/pages/calendar/AddTimeModal.tsx
+++ src/pages/calendar/AddTimeModal.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -9,16 +8,23 @@ import {
   Button,
   Box,
 } from "@mui/material";
-import { DateTimeField } from "@mui/x-date-pickers/DateTimeField";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import moment from "moment";
+import moment, { Moment } from "moment";
+import axios from "axios";
 
 interface AddTimeModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (newEvent: { user_name: string; start_time: string; end_time: string }) => void;
+  onSubmit: (event: {
+    id?: number;
+    user_name: string;
+    start_time: string;
+    end_time: string;
+  }) => void;
   selectedDate: { start: Date; end: Date } | null;
+  eventToEdit?: { id?: number; title: string; start: Date; end: Date } | null;
 }
 
 const AddTimeModal: React.FC<AddTimeModalProps> = ({
@@ -26,40 +32,74 @@ const AddTimeModal: React.FC<AddTimeModalProps> = ({
   onClose,
   onSubmit,
   selectedDate,
+  eventToEdit,
 }) => {
-  const [userName, setUserName] = useState<string>("");
-  const [startTime, setStartTime] = useState<any>(null);
-    const [endTime, setEndTime] = useState<any>(null);
+  let backendUrl = import.meta.env.VITE_BACKEND_URL;
+  if (import.meta.env.VITE_ENV === "production") {
+    backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
+  }
 
-  // Handle input changes
+  const [userName, setUserName] = useState<string>("");
+  const [startTime, setStartTime] = useState<Moment | null>(null);
+  const [endTime, setEndTime] = useState<Moment | null>(null);
+
+  useEffect(() => {
+    if (eventToEdit) {
+      setUserName(eventToEdit.title);
+      setStartTime(moment(eventToEdit.start));
+      setEndTime(moment(eventToEdit.end));
+    } else if (selectedDate) {
+      setUserName("");
+      setStartTime(moment(selectedDate.start));
+      setEndTime(moment(selectedDate.end));
+    } else {
+      setUserName("");
+      setStartTime(moment().startOf("day"));
+      setEndTime(moment().startOf("day"));
+    }
+  }, [eventToEdit, selectedDate]);
+
   const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   };
 
-  const handleStartTimeChange = (newValue: Date | null) => {
-    setStartTime(newValue);
-  };
+  const handleDelete = async () => {
+    if (eventToEdit && eventToEdit.id) {
+      try {
+        const token = localStorage.getItem("accessToken");
+        const response = await axios.delete(
+          `${backendUrl}/api/calendar/delete-availability/${eventToEdit.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-  const handleEndTimeChange = (newValue: Date | null) => {
-    setEndTime(newValue);
+        if (response.status === 200) {
+          onClose();
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error("Error deleting availability:", error);
+        alert("Failed to delete availability");
+      }
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
     if (userName && startTime && endTime) {
       try {
-        const formattedStartTime = moment(startTime).toISOString();
-        const formattedEndTime = moment(endTime).toISOString();
-
         onSubmit({
+          id: eventToEdit?.id,
           user_name: userName,
-          start_time: formattedStartTime,
-          end_time: formattedEndTime,
+          start_time: startTime.format("YYYY-MM-DDTHH:mm:ss"),
+          end_time: endTime.format("YYYY-MM-DDTHH:mm:ss"),
         });
-        onClose(); // Close modal after submitting
+        onClose();
       } catch (error) {
-        console.error("Error adding availability:", error);
+        console.error("Error adding/updating availability:", error);
       }
     } else {
       alert("Please fill in all fields.");
@@ -73,44 +113,100 @@ const AddTimeModal: React.FC<AddTimeModalProps> = ({
       className="custom-modal"
       sx={{
         "& .MuiPaper-root": {
-          maxWidth: "500px",
+          maxWidth: "540px",
           width: "100%",
         },
       }}
     >
-      <DialogTitle>Add Availability</DialogTitle>
+      <DialogTitle>
+        {eventToEdit ? "Edit availability" : "Add availability"}
+      </DialogTitle>
       <DialogContent>
         <LocalizationProvider dateAdapter={AdapterMoment}>
           <Box className="form-fields">
             <TextField
               label="Name"
               name="name"
-              value={userName}
+              value={eventToEdit?.title ? eventToEdit.title : userName}
               onChange={handleUserNameChange}
               fullWidth
               variant="filled"
-              margin="normal"
             />
-            <DateTimeField
-                label="Start time"
-                variant="filled"
-                onChange={(newValue) => setStartTime(newValue)}
+            <DateTimePicker
+              label="Start time"
+              value={startTime}
+              defaultValue={
+                selectedDate
+                  ? moment(selectedDate.start).startOf("day")
+                  : moment().startOf("day")
+              }
+              onChange={(newValue) =>
+                setStartTime(newValue ? moment(newValue) : null)
+              }
+              format="DD.MM.YYYY HH:mm"
+              ampm={false}
+              slotProps={{
+                textField: {
+                  placeholder: "",
+                  InputLabelProps: { shrink: false },
+                },
+              }}
+              sx={{
+                width: "100%",
+                ".MuiInputLabel-root": {
+                  display: "none",
+                },
+                ".MuiOutlinedInput-notchedOutline": {
+                  border: 0,
+                },
+              }}
             />
-            <DateTimeField
-                label="End time"
-                variant="filled"
-                onChange={(newValue) => setEndTime(newValue)}
+            <DateTimePicker
+              label="End time"
+              value={endTime}
+              defaultValue={
+                selectedDate
+                  ? moment(selectedDate.end).startOf("day")
+                  : moment().startOf("day")
+              }
+              onChange={(newValue) => setEndTime(moment(newValue))}
+              format="DD.MM.YYYY HH:mm"
+              ampm={false}
+              minDateTime={startTime || undefined}
+              slotProps={{
+                textField: {
+                  placeholder: "",
+                  InputLabelProps: { shrink: false },
+                },
+              }}
+              sx={{
+                width: "100%",
+                ".MuiInputLabel-root": {
+                  display: "none",
+                },
+              }}
             />
           </Box>
         </LocalizationProvider>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} className="cancel-btn">
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit} className="submit-btn">
-          Add
-        </Button>
+      <DialogActions sx={{ display: "flex", justifyContent: "space-between" }}>
+        {eventToEdit && (
+          <Button className="delete-btn" onClick={handleDelete}>
+            Delete availability
+          </Button>
+        )}
+        <div>
+          <Button onClick={onClose} className="cancel-btn">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleSubmit}
+            className="submit-btn"
+            disabled={!userName || !startTime || !endTime}
+          >
+            {eventToEdit ? "Update" : "Add"}
+          </Button>
+        </div>
       </DialogActions>
     </Dialog>
   );
--- src/pages/calendar/CalendarPage.tsx
+++ src/pages/calendar/CalendarPage.tsx
@@ -6,114 +6,189 @@ import axios from "axios";
 
 import AddTimeModal from "./AddTimeModal";
 
+moment.locale("en-GB", {
+  week: {
+    dow: 1,
+  },
+});
 const localizer = momentLocalizer(moment);
 
 const CalendarPage = () => {
-    let backendUrl = import.meta.env.VITE_BACKEND_URL;
-    if (import.meta.env.VITE_ENV === "production") {
-      backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
-    }
+  let backendUrl = import.meta.env.VITE_BACKEND_URL;
+  if (import.meta.env.VITE_ENV === "production") {
+    backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
+  }
 
-    const [events, setEvents] = useState<{ id?: number; title: string; start: Date; end: Date }[]>([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedDate, setSelectedDate] = useState<{ start: Date; end: Date } | null>(null);
+  const [events, setEvents] = useState<
+    { id?: number; title: string; start: Date; end: Date }[]
+  >([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<{
+    start: Date;
+    end: Date;
+  } | null>(null);
+  const [selectedEvent, setSelectedEvent] = useState<{
+    id?: number;
+    title: string;
+    start: Date;
+    end: Date;
+  } | null>(null);
 
-    useEffect(() => {
-        const fetchAvailability = async () => {
-          try {
-            const token = localStorage.getItem("accessToken");
-            const response = await axios.get(`${backendUrl}/api/calendar`, {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
-            if (response && response.data && response.data.availability) {
-                const formattedEvents = response.data.availability[0].map((availability: { id: number; user_name: string; start_time: string | number | Date; end_time: string | number | Date; }) => ({
-                  id: availability.id,
-                  title: `${availability.user_name} - Available`,
-                  start: new Date(availability.start_time),
-                  end: new Date(availability.end_time),
-                }));
-                setEvents(formattedEvents);
-            }
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-    
-        fetchAvailability();
-    }, [backendUrl]);
-
-    const handleSelectSlot = ({ start, end }: { start: Date; end: Date }) => {
-        setSelectedDate({ start, end });
-        setIsModalOpen(true);
-      };
-
-      const handleCloseModal = () => {
-        setIsModalOpen(false);
-      };
-    
-      // Handle new event submission
-      const handleSubmitNewEvent = async (newEvent: { user_name: string; start_time: string; end_time: string }) => {
-        const { user_name, start_time, end_time } = newEvent;
-        try {
-          const token = localStorage.getItem("accessToken");
-          await axios.post(
-            `${backendUrl}/api/calendar`,
-            { user_name, start_time, end_time },
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
+  useEffect(() => {
+    const fetchAvailability = async () => {
+      try {
+        const token = localStorage.getItem("accessToken");
+        const response = await axios.get(`${backendUrl}/api/calendar`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response && response.data && response.data.availability) {
+          const formattedEvents = response.data.availability[0].map(
+            (availability: {
+              id: number;
+              user_name: string;
+              start_time: string | number | Date;
+              end_time: string | number | Date;
+            }) => ({
+              id: availability.id,
+              title: `${availability.user_name}`,
+              start: new Date(availability.start_time),
+              end: new Date(availability.end_time),
+            })
           );
-          setEvents([
-            ...events,
-            {
-              title: `${user_name} - Available`,
-              start: new Date(start_time),
-              end: new Date(end_time),
-            },
-          ]);
-        } catch (error) {
-          console.error("Error adding availability:", error);
+          setEvents(formattedEvents);
         }
-      };
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-    return (
+    fetchAvailability();
+  }, [backendUrl]);
+
+  const handleSelectSlot = ({ start, end }: { start: Date; end: Date }) => {
+    setSelectedDate({ start, end });
+    setSelectedEvent(null);
+    setIsModalOpen(true);
+  };
+
+  const handleSelectEvent = (event: {
+    id?: number;
+    title: string;
+    start: Date;
+    end: Date;
+  }) => {
+    setSelectedEvent(event);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedEvent(null);
+    setIsModalOpen(false);
+  };
+
+  const handleSubmitNewEvent = async (newEvent: {
+    id?: number;
+    user_name: string;
+    start_time: string;
+    end_time: string;
+  }) => {
+    const { user_name, start_time, end_time, id } = newEvent;
+
+    try {
+      const token = localStorage.getItem("accessToken");
+
+      if (id) {
+        await axios.put(
+          `${backendUrl}/api/calendar/${id}`,
+          { user_name, start_time, end_time },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setEvents(
+          events.map((event) =>
+            event.id === id
+              ? {
+                  ...event,
+                  title: user_name,
+                  start: new Date(start_time),
+                  end: new Date(end_time),
+                }
+              : event
+          )
+        );
+        window.location.reload();
+      } else {
+        await axios.post(
+          `${backendUrl}/api/calendar`,
+          { user_name, start_time, end_time },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setEvents([
+          ...events,
+          {
+            title: user_name,
+            start: new Date(start_time),
+            end: new Date(end_time),
+          },
+        ]);
+        window.location.reload();
+      }
+
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error("Error adding/updating availability:", error);
+    }
+  };
+
+  return (
     <>
-    <section>
+      <section style={{ paddingBottom: "80px" }} className="custom-calendar">
         <div className="wrapper">
-            <div className="row">
-                <div className="col-12">
-                    <div style={{ height: "700px" }}>
-                    <h2>Availability Calendar</h2>
-                    <Calendar
-                        localizer={localizer}
-                        events={events}
-                        startAccessor="start"
-                        endAccessor="end"
-                        step={30}
-                        defaultView="month"
-                        min={new Date(2025, 1, 1, 8, 0)}
-                        max={new Date(2025, 1, 1, 21, 0)} 
-                        style={{ height: "100%" }}
-                        selectable
-                        onSelectSlot={handleSelectSlot}
-                    />
-                    </div>
-                </div>
+          <div className="row">
+            <div className="col-12">
+              <div style={{ height: "660px" }}>
+                <Calendar
+                  localizer={localizer}
+                  culture="en-GB"
+                  events={events}
+                  startAccessor="start"
+                  endAccessor="end"
+                  step={30}
+                  defaultView="month"
+                  min={new Date(2025, 1, 1, 7, 0)}
+                  max={new Date(2025, 1, 1, 21, 0)}
+                  style={{ height: "100%" }}
+                  views={{
+                    day: true,
+                    month: true,
+                  }}
+                  selectable
+                  onSelectEvent={handleSelectEvent}
+                  onSelectSlot={handleSelectSlot}
+                />
+              </div>
             </div>
+          </div>
         </div>
-    </section>
-    <AddTimeModal
+      </section>
+      <AddTimeModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
         onSubmit={handleSubmitNewEvent}
-        selectedDate={selectedDate}
+        selectedDate={selectedEvent || selectedDate}
+        eventToEdit={selectedEvent}
       />
     </>
-    );
+  );
 };
 
 export default CalendarPage;
