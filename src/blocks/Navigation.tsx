d20125c19c4e3082ddf3c73d1cdc5f0c1902cb3e
Mario <mariovidovic97@gmail.com>
2025-02-15 14:03:58 +0100

    Navigation - template page added

--- src/blocks/Navigation.tsx
+++ src/blocks/Navigation.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import axios from "axios";
 
 import LogoImage from "../assets/logo.svg";
 import FullscreenIcon from "../assets/icons/fullscreen.svg";
@@ -12,8 +13,14 @@ import {
   Avatar,
   Divider,
 } from "@mui/material";
+import AddTemplateModal from "../pages/templates/AddTemplateModal";
 
 const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
+  let backendUrl = import.meta.env.VITE_BACKEND_URL;
+  if (import.meta.env.VITE_ENV === "production") {
+    backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
+  }
+
   const { user, setUser } = useUser();
   const navigate = useNavigate();
   const location = useLocation();
@@ -28,9 +35,14 @@ const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
     /^\/incident\/[^/]+($|\/edit$)/.test(cleanPathname);
   const addSpace = cleanPathname === "/";
   const isIncidentPage = cleanPathname === "/incidents";
+  const isTemplatesPage = cleanPathname === "/templates";
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const openMenu = Boolean(anchorEl);
+
+  const [openTemplateModal, setOpenTemplateModal] = useState(false);
+  const [templateTitle, setTemplateTitle] = useState("");
+  const [templateContent, setTemplateContent] = useState("");
 
   const [isFullscreen, setIsFullscreen] = useState(false);
   const toggleFullscreen = () => {
@@ -68,6 +80,30 @@ const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
   };
   const handleTemplatesPage = () => {
     navigate("/templates");
+  };
+
+  const handleOpenTemplateModal = () => {
+    setOpenTemplateModal(true);
+  };
+  const handleCloseModal = () => setOpenTemplateModal(false);
+  const handleCreateTemplate = async () => {
+    const newTemplate = { title: templateTitle, content: templateContent };
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await axios.post(
+        `${backendUrl}/api/templates`,
+        newTemplate,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      if (response && response.data) {
+      }
+      if (response.status === 201) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error adding contact:", error);
+      alert("Error adding contact.");
+    }
   };
 
   return (
@@ -113,10 +149,53 @@ const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
           <div className="col-12">
             {!isSpecialPage && (
               <>
-                {isIncidentPage ? (
-                  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                {isIncidentPage || isTemplatesPage ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
                     <h1>{pageTitle}</h1>
-                    <button onClick={handleTemplatesPage} style={{ display: 'inline-flex', alignItems: 'center', height: '48px', fontSize: '15px', fontWeight: '600', padding: '0px 24px', border: '1px solid #f5f5f5', borderRadius: '6px', backgroundColor: '#f5f5f5', color: '#1b2431' }}>Templates</button>
+                    {isIncidentPage && (
+                      <button
+                        onClick={handleTemplatesPage}
+                        style={{
+                          display: "inline-flex",
+                          alignItems: "center",
+                          height: "48px",
+                          fontSize: "15px",
+                          fontWeight: "600",
+                          padding: "0px 24px",
+                          border: "1px solid #f5f5f5",
+                          borderRadius: "6px",
+                          backgroundColor: "#f5f5f5",
+                          color: "#1b2431",
+                        }}
+                      >
+                        Templates
+                      </button>
+                    )}
+                    {isTemplatesPage && (
+                      <button
+                        onClick={handleOpenTemplateModal}
+                        style={{
+                          display: "inline-flex",
+                          alignItems: "center",
+                          height: "48px",
+                          fontSize: "15px",
+                          fontWeight: "600",
+                          padding: "0px 24px",
+                          border: "1px solid #f5f5f5",
+                          borderRadius: "6px",
+                          backgroundColor: "#f5f5f5",
+                          color: "#1b2431",
+                        }}
+                      >
+                        Add new
+                      </button>
+                    )}
                   </div>
                 ) : (
                   <h1>{pageTitle}</h1>
@@ -132,9 +211,7 @@ const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
         anchorEl={anchorEl}
         open={openMenu}
         onClose={handleMenuClose}
-        MenuListProps={{
-          "aria-labelledby": "avatar-button",
-        }}
+        MenuListProps={{ "aria-labelledby": "avatar-button" }}
         className="avatar-menu"
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
@@ -146,11 +223,26 @@ const Navigation = ({ pageTitle }: { pageTitle?: string }) => {
           <p>{user?.email}</p>
         </Box>
         <Divider />
-        <MenuItem onClick={handleAccountSettings}>Account</MenuItem>
-        <MenuItem onClick={handleNotificationsPage}>Notifications</MenuItem>
+        <MenuItem sx={{ minHeight: "unset" }} onClick={handleAccountSettings}>
+          Account
+        </MenuItem>
+        <MenuItem sx={{ minHeight: "unset" }} onClick={handleNotificationsPage}>
+          Notifications
+        </MenuItem>
         <Divider />
-        <MenuItem onClick={handleLogout}>Log out</MenuItem>
+        <MenuItem sx={{ minHeight: "unset" }} onClick={handleLogout}>
+          Log out
+        </MenuItem>
       </Menu>
+      <AddTemplateModal
+        open={openTemplateModal}
+        onClose={handleCloseModal}
+        onSubmit={handleCreateTemplate}
+        templateTitle={templateTitle}
+        templateContent={templateContent}
+        setTemplateTitle={setTemplateTitle}
+        setTemplateContent={setTemplateContent}
+      />
     </nav>
   );
 };
