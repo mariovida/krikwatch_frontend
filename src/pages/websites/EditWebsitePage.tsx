3440f38aa6a7f02c5bf243d539c3f5c666da6113
Mario <mariovidovic97@gmail.com>
2025-02-17 16:33:21 +0100

    Edit website improved

--- src/pages/websites/EditWebsitePage.tsx
+++ src/pages/websites/EditWebsitePage.tsx
@@ -38,6 +38,9 @@ const EditWebsitePage = () => {
   const [uptimeId, setUptimeId] = useState<string>("");
   const [hostingInfo, setHostingInfo] = useState<string>("");
   const [hostingUrl, setHostingUrl] = useState<string>("");
+  const [favicon, setFavicon] = useState<File | null>(null);
+  const [faviconPreview, setFaviconPreview] = useState<string | null>(null);
+  const [faviconChanged, setFaviconChanged] = useState<boolean>(false);
 
   const [openConfirmModal, setOpenConfirmModal] = useState(false);
   const [showError, setShowError] = useState<boolean>(false);
@@ -59,6 +62,11 @@ const EditWebsitePage = () => {
           setUptimeId(response.data.website.uptime_id);
           setHostingUrl(response.data.website.hosting_url);
           setHostingInfo(response.data.website.hosting_info);
+          if (response.data.website.favicon) {
+            setFaviconPreview(
+              `data:image/png;base64,${response.data.website.favicon}`
+            );
+          }
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -85,12 +93,48 @@ const EditWebsitePage = () => {
     }
   }, [backendUrl, id]);
 
+  const uploadFavicon = async (file: File): Promise<string | null> => {
+    const formData = new FormData();
+    formData.append("favicon", file);
+
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await axios.post(
+        `${backendUrl}/api/websites/upload-favicon`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.status === 200 && response.data.fileName) {
+        return response.data.fileName;
+      } else {
+        throw new Error("File upload failed");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      return null;
+    }
+  };
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     if (!websiteName || !selectedClient) {
       setShowError(true);
       return;
+    }
+
+    let uploadedFavicon = website?.favicon;
+    if (favicon && faviconChanged) {
+      uploadedFavicon = await uploadFavicon(favicon);
+    }
+    if (!favicon && faviconChanged) {
+      uploadedFavicon = null;
     }
 
     const updatedWebsite = {
@@ -100,6 +144,8 @@ const EditWebsitePage = () => {
       uptime_id: uptimeId,
       hosting_url: hostingUrl,
       hosting_info: hostingInfo,
+      favicon_changed: faviconChanged,
+      favicon: uploadedFavicon,
     };
 
     try {
@@ -116,7 +162,7 @@ const EditWebsitePage = () => {
       );
 
       if (response.status === 200) {
-        navigate(`/websites`);
+        navigate(`/website/${website.id}`);
       }
     } catch (error) {
       console.error("Error updating website:", error);
@@ -152,7 +198,7 @@ const EditWebsitePage = () => {
         <title>{website ? website.name : "Edit website"} | KrikWatch</title>
       </Helmet>
 
-      <section>
+      <section style={{ paddingBottom: "80px" }}>
         <div className="wrapper">
           <div className="row">
             <div className="col-12 col-md-8 offset-md-2">
@@ -171,6 +217,87 @@ const EditWebsitePage = () => {
               </Typography>
               <form onSubmit={handleSubmit} className="custom-form">
                 <Box className="form-fields" sx={{ marginTop: "0 !important" }}>
+                  <Typography
+                    sx={{
+                      fontFamily: "Plus Jakarta Sans, sans-serif",
+                      fontSize: "18px",
+                      fontWeight: 700,
+                      cursor: "default",
+                    }}
+                  >
+                    Website favicon
+                  </Typography>
+                  {!faviconPreview ? (
+                    <Box>
+                      <input
+                        accept="image/*"
+                        id="favicon-upload"
+                        type="file"
+                        style={{ display: "none" }}
+                        onChange={(e) => {
+                          if (e.target.files && e.target.files[0]) {
+                            const file = e.target.files[0];
+                            setFavicon(file);
+                            setFaviconPreview(URL.createObjectURL(file));
+                            setFaviconChanged(true);
+                          }
+                        }}
+                      />
+                      <label htmlFor="favicon-upload" style={{ width: "100%" }}>
+                        <Button
+                          component="span"
+                          sx={{
+                            width: "100%",
+                            height: "80px",
+                            backgroundColor: "transparent",
+                            color: "#7e7e7e",
+                            fontFamily: "Plus Jakarta Sans, sans-serif",
+                            fontSize: "16px",
+                            fontWeight: 400,
+                            letterSpacing: 0,
+                            textTransform: "none",
+                            padding: "0 16px",
+                            border: "1px solid #E5E7EB",
+                            borderRadius: "8px",
+                            "&:hover": { backgroundColor: "#f2f2f2" },
+                          }}
+                        >
+                          Select favicon
+                        </Button>
+                      </label>
+                    </Box>
+                  ) : (
+                    <>
+                      <Button
+                        color="error"
+                        onClick={() => {
+                          setFavicon(null);
+                          setFaviconPreview(null);
+                          setFaviconChanged(true);
+                        }}
+                        sx={{
+                          fontSize: "15px",
+                          textTransform: "none",
+                          fontWeight: 600,
+                          letterSpacing: 0,
+                          color: "#ffffff",
+                          backgroundColor: "#d84141",
+                          padding: "6px 8px",
+                        }}
+                      >
+                        Remove
+                      </Button>
+                      <img
+                        src={faviconPreview}
+                        alt="Favicon Preview"
+                        style={{
+                          width: "80px",
+                          height: "80px",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </>
+                  )}
                   <Typography
                     sx={{
                       fontFamily: "Plus Jakarta Sans, sans-serif",
